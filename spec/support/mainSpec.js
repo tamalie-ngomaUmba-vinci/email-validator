108f63d4362b5fdc9c987c9f2f4fbcf4f476940c
tamalie-ngomaUmba-vinci <succes.ngomaumbatama@student.vinci.be>
2025-08-28 16:31:22 +0200

    ajout d'un test

--- spec/support/mainSpec.js
+++ spec/support/mainSpec.js
@@ -1,3 +1,9 @@
 import { Main } from '../../src/main.js';
 
 const main = new Main();
+
+describe('Email Validator', () => {
+    it("reponse attendue : true", function() {
+        expect(main.validateEmail("trea@faoiu.be")).toBe(true);
+});
+});
