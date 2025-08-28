5595ae640eb857e1cbc5afa459c7396e555125e7
tamalie-ngomaUmba-vinci <succes.ngomaumbatama@student.vinci.be>
2025-08-28 16:54:07 +0200

    changement test pour éviter erreur de deploiement

--- src/main.js
+++ src/main.js
@@ -2,16 +2,7 @@ export class Main {
 
     emailValidator(email) {
         
-        emailValide = false;
-
-        pasVide(email);{
-            emailValide = emailValide && (email !== null && email !== undefined && email.length > 0);
-        }
-       /* aUnarobase(email){
-            emailValide = e && email.includes('@');
-        } */
-
-        
+        return email.includes('@') && email.includes('.');      
         
 
     }
